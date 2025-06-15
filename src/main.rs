b2dcd6d82e8dc6d05fb019a4a5bd07129a5bcd6c
Amos KIM <metaskyer@gmail.com>
2025-06-15 18:48:24 +0900

    generic types

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,13 @@
-use std::env;
-
 fn main() {
-    // 벡터 타입 데이터로 변수 선언
-    let args: Vec<String> = env::args().collect();
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    let config = parse_config(&args);
-    
-    // let query =  &args[1];
+    let mut largest = &number_list[0];
 
-    // let filename = &args[2];
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
 
-    // 한글로 변경시도
-    println!("검색: {}", config.query);
-    println!("대상 파일 : {}", config.filename);
-
-    let contents = fs::read_to_string(config.filename).expect("파일을 읽지 못했습니다.");
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config {query, filename}
+    println!("The largest number is {largest}");
 }
