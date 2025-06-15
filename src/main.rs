d96a9024c20d0b0a62df38acf331dee4274944d0
Amos KIM <metaskyer@gmail.com>
2025-06-15 10:38:01 +0900

    edit_config

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,28 @@ use std::env;
 fn main() {
     // 벡터 타입 데이터로 변수 선언
     let args: Vec<String> = env::args().collect();
-    
-    let query =  &args[1];
 
-    let filename = &args[2];
+    let config = parse_config(&args);
+    
+    // let query =  &args[1];
+
+    // let filename = &args[2];
 
     // 한글로 변경시도
-    println!("검색: {}", query);
-    println!("파일의 이름은 : {}", filename);
+    println!("검색: {}", config.query);
+    println!("대상 파일 : {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("파일을 읽지 못했습니다.");
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {query, filename}
 }
