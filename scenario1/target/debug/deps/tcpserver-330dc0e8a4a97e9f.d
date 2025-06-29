e3b7c798b32dbbec5e03352706a78a360551be79
Amos KIM <metaskyer@gmail.com>
2025-06-29 15:39:15 +0900

    s-s-start2

---
+++ scenario1/target/debug/deps/tcpserver-330dc0e8a4a97e9f.d
@@ -0,0 +1,5 @@
+/Users/kimhongnam/projects/git_rust/rust-git/scenario1/target/debug/deps/tcpserver-330dc0e8a4a97e9f: tcpserver/src/main.rs
+
+/Users/kimhongnam/projects/git_rust/rust-git/scenario1/target/debug/deps/tcpserver-330dc0e8a4a97e9f.d: tcpserver/src/main.rs
+
+tcpserver/src/main.rs:
---
+++ scenario1/target/debug/tcpserver.d
@@ -0,0 +1,1 @@
+/Users/kimhongnam/projects/git_rust/rust-git/scenario1/target/debug/tcpserver: /Users/kimhongnam/projects/git_rust/rust-git/scenario1/tcpserver/src/main.rs
